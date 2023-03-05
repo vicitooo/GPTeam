43c86c8c70d10c903b4e02bc40be54073448969c
victor.valtchev <victor.valtchev@telerikacademy.com>
2023-03-05 09:35:02 +0200

    Add GPTeam Logic

--- src/main/java/com/example/demo/controllers/mvc/PageControllers.java
+++ src/main/java/com/example/demo/controllers/mvc/PageControllers.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class PageControllers {
 
-    @RequestMapping("/")
+    @RequestMapping("/chat")
     public String index() {
         return "home";
     }
