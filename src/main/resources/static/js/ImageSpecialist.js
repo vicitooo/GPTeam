0f608d088f59cf0e41bf6a5e4bef50d1b79bbdab
victor.valtchev <victor.valtchev@telerikacademy.com>
2023-03-07 21:23:59 +0200

    Add Image Generator.

--- src/main/resources/static/js/ImageSpecialist.js
+++ src/main/resources/static/js/ImageSpecialist.js
@@ -1,41 +1,43 @@
 document.addEventListener('DOMContentLoaded', function() {
-    const form = document.getElementById('generate-image-form');
-  
-    form.addEventListener('submit', function(event) {
-      event.preventDefault();
-  
-      const input = document.getElementById('image-prompt').value;
-  
-      // API endpoint URL
-      const url = 'https://api.openai.com/v1/images/generations';
-  
-      // Request parameters
-      const params = {
-        prompt: input,
-        n: 1,
-        size: '512x512',
-      };
-  
-      // API request options
-      const options = {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-          'Authorization': 'Bearer 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',
-        },
-        body: JSON.stringify(params),
-      };
-  
-      // Send API request
-      fetch(url, options)
-        .then(response => response.json())
-        .then(data => {
-          const imageUrl = data.data[0].url;
-          const image = document.createElement('img');
-          image.src = imageUrl;
-          document.getElementById('generated-image').appendChild(image);
-        })
-        .catch(error => console.error(error));
+const form = document.getElementById('generate-image-form');
+
+form.addEventListener('submit', async function(event) {
+  event.preventDefault();
+
+  const input = document.getElementById('image-prompt').value;
+
+  try {
+    const response = await fetch('https://us-central1-api-project-1058745121639.cloudfunctions.net/image/generate', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({ prompt: input })
     });
-  });
-  
+
+    if (!response.ok) {
+      throw new Error(`HTTP error! status: ${response.status}`);
+    }
+
+    // Response is a JSON object
+    // {
+        // "created": 1589478378,
+        // "data": [
+        //   {
+            // "url": "https://..."
+        //   },
+        //   {
+            // "url": "https://..."
+        //   }
+        // ]
+    //   }
+      
+    const data = await response.json();
+    const image = data.data[0].url;
+
+    const imageContainer = document.getElementById('generated-image');
+    imageContainer.innerHTML = `<img src="${image}" />`;
+      } catch (error) {
+    console.error(error);
+  }
+});});
